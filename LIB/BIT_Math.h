dddbf53e252cf3c6c224e7f05f60ca3bd9443d10
Peter Essam <75110347+peteressam23@users.noreply.github.com>
2023-10-08 12:28:55 +0300

    First Commit
    Libraries

---
+++ LIB/BIT_Math.h
@@ -0,0 +1,12 @@
+
+#ifndef SERVIES_BIT_MATH_H_
+#define SERVIES_BIT_MATH_H_
+
+
+#define SET_BIT(REG,BIT_NUMBER) 	(REG |= (1<<BIT_NUMBER))
+#define GET_BIT(REG,BIT_NUMBER)		((REG>>BIT_NUMBER)&1)
+#define CLEAR_BIT(REG,BIT_NUMBER) 	(REG &= (~(1<<BIT_NUMBER)))
+#define TOGGLE_BIT(REG,BIT_NUMBER) 	(REG ^=(1<<BIT_NUMBER))
+
+
+#endif /* SERVIES_BIT_MATH_H_ */
---
+++ LIB/STD_Types.h
@@ -0,0 +1,28 @@
+/*
+ * std.h
+ *
+ * Created: 10/3/2023 10:22:02 AM
+ *  Author: pessam
+ */ 
+
+#ifndef STD_H_
+#define STD_H_
+
+/*********************************************Macros of Data Types*********************************************************/
+
+
+typedef unsigned char       u8;             // 1 byte unsigned char
+typedef signed   char       s8;            // 1 byte signed char
+typedef unsigned short int  u16;          // 2 byte unsigned int
+typedef signed   short int  s16;         // 2 byte signed int
+typedef unsigned long int   u32;        // 4 byte unsigned long int
+typedef signed   long int   s32;       // 4 byte signed long int
+typedef float               f32;      // 4 byte float
+typedef double              f64;     // 8 byte  double
+#define NULL 			     0
+#define NULL_PTR		  ((void*)0)
+
+
+
+
+#endif /* STD_H_ */
